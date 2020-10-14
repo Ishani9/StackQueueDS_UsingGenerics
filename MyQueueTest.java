e4c33079257dc3a2d56cb4713d0aadab051e312c
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-14 16:55:56 +0530

    Added: Ability to dequeue from the beginning

--- MyQueueTest.java
+++ MyQueueTest.java
@@ -32,5 +32,29 @@ public class MyQueueTest {
 		Assert.assertEquals(myThirdNode, myQueue.getTail());
 	}
 
+	
+	//UC 2
+	@Test
+	public void given3Numbers_whenDequeued_shouldReturnFirst() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyQueue myQueue = new MyQueue();
+		myQueue.enqueue(myFirstNode);
+		myQueue.enqueue(mySecondNode);
+		myQueue.enqueue(myThirdNode);
+		System.out.println("Current queue is : ");
+		myQueue.printQueue();
+		Assert.assertEquals(myFirstNode, myQueue.dequeue());
+		System.out.println("Current queue after dequeue is : ");
+		myQueue.printQueue();
+		Assert.assertEquals(mySecondNode, myQueue.dequeue());
+		System.out.println("Current queue after second dequeue is : ");
+		myQueue.printQueue();
+		Assert.assertEquals(myThirdNode, myQueue.dequeue());
+		System.out.println("Current queue after third dequeue is : ");
+		myQueue.printQueue();
+	}
+
 
 }
